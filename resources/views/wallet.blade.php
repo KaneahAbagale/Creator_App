d306a2c9cea854972e3ffa60b55ecd1398d2570e
KaneahAbagale <gertrude.kaneah.abagale@gmail.com>
2022-05-02 23:28:24 +0000

    relocated auth

--- resources/views/wallet.blade.php
+++ resources/views/wallet.blade.php
@@ -2,7 +2,7 @@
   <div>    
     <div class="p-2">
       <div class="flex items-center flex-wrap ">
-        {{Auth::user()->wallet ? Auth::user()->wallet->amount : 0}}
+        
         <a href="content/create">Wallet</a>
       </div>
     </div>
@@ -11,7 +11,8 @@
       <div class="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
       <!-- <img class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100" src="https://images.unsplash.com/photo-1618172193622-ae2d025f4032?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80" alt="blog"> -->
       <div class="p-6">
-          <h1 class="title-font text-lg font-medium text-gray-600 mb-3">History</h1>
+          <h1 class="title-font text-lg font-medium text-gray-600 mb-3"></h1>
+          {{Auth::user()->wallet ? Auth::user()->wallet->amount : 0}}
           <p class="leading-relaxed mb-3">Balance</p>
           <div class="flex items-center flex-wrap ">
             <a href="wallet" class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Payout</a>
