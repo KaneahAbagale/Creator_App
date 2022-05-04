ec305ddc3f5fb434cd13a9c9919aa08bf20b39c7
KaneahAbagale <gertrude.kaneah.abagale@gmail.com>
2022-05-04 22:02:49 +0000

    index

--- resources/views/payout/index.blade.php
+++ resources/views/payout/index.blade.php
@@ -28,7 +28,7 @@
 
                             <div class="grid grid-cols-3 gap-6">
                                 <div class="col-span-3 sm:col-span-2">
-                                    <label for="name" value="{{ __('payout.index') }}" class="block text-sm font-medium text-gray-700">Title</label>
+                                    <label for="name" class="block text-sm font-medium text-gray-700">Title</label>
                                     <div class="mt-1 flex rounded-md shadow-sm">
                                         <input name="title" id="contenttype" type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Art/Music title">           
                                     </div>
@@ -37,7 +37,7 @@
                                 
                             <div class="grid grid-cols-3 gap-6">
                                 <div class="col-span-3 sm:col-span-2">
-                                    <label for="name" value="{{ __('payout.index') }}" class="block text-sm font-medium text-gray-700">Title</label>
+                                    <label for="name" class="block text-sm font-medium text-gray-700">Title</label>
                                     <div class="mt-1 flex rounded-md shadow-sm">
                                         <input name="title" id="contenttype" type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Art/Music title">           
                                     </div>
@@ -46,29 +46,16 @@
         
                             <div class="grid grid-cols-3 gap-6">
                                 <div class="col-span-3 sm:col-span-2">
-                                    <label for="name" value="{{ __('payout.index') }}" class="block text-sm font-medium text-gray-700">Title</label>
+                                    <label for="name" class="block text-sm font-medium text-gray-700">Title</label>
                                     <div class="mt-1 flex rounded-md shadow-sm">
                                         <input name="title" id="contenttype" type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Art/Music title">           
                                     </div>
                                 </div>
                             </div> 
 
-                                <div>
-                                    <label for="about" class="block text-sm font-medium text-gray-700"> Description</label>
-                                    <div class="mt-1">
-                                    <textarea id="about" name="description" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Eg. label name, year"></textarea>
-                                    </div>
-                                    <p class="mt-2 text-sm text-gray-500">Brief description of your Content. URLs are hyperlinked.</p>
-                                </div>
+                            
 
                                 <div>
-                                    <label class="block text-sm font-medium text-gray-700"> Content File </label>
-                                    <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                                    <div class="space-y-1 text-center">
-                                        <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                                        <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                                        </svg>
-                                    </div>
                                     <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
                                         <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Pay Out</button>
                                     </div>    
