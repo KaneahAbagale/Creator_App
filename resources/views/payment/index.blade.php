998a499d779c5895f569e5bcaef5f2902cb8ab83
KaneahAbagale <gertrude.kaneah.abagale@gmail.com>
2022-05-04 16:03:04 +0000

    updates in wallet and index

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -8,9 +8,6 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-
     </head>
     <body >
         <form id="paymentForm">
