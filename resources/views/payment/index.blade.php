0df7f9cf0355111e0251cd57bb7981fa459c6a8d
KaneahAbagale <gertrude.kaneah.abagale@gmail.com>
2022-05-05 10:29:09 +0000

    indentation

--- resources/views/payment/index.blade.php
+++ resources/views/payment/index.blade.php
@@ -1,57 +1,59 @@
 <x-app-layout>
-    <!DOCTYPE html>
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
+    <div class="p-24">
+        <!DOCTYPE html>
+        <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
 
-            <title>Paystack Payment </title>
+                <title>Paystack Payment </title>
 
-            <!-- Fonts -->
-            <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        </head>
-        <body >
-            <form id="paymentForm">
-                <input value="abagale1994@gmail.com" type="email" id="email-address" hidden />
-                <div class="form-group">
-                <label for="amount">Amount</label>
-                <input value="{{$content->price ?? 0.00}}" type="text" id="amount" disabled />
-                </div>
-                
-                <div class="form-submit">
-                <button type="submit" onclick="payWithPaystack(event)"> Pay </button>
-                </div>
-            </form>
-            <script src="https://js.paystack.co/v1/inline.js"></script> 
-            <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
-            <script>
-                let paymentForm = document.getElementById('paymentForm');
-                paymentForm.addEventListener("submit", payWithPaystack, false);
-                function payWithPaystack(e) {
-                e.preventDefault();
-                let handler = PaystackPop.setup({
-                    key: 'pk_test_cef059417db5c00edff1ee70216ed36dd4ffb4e0', // Replace with your public key
-                    email: document.getElementById("email-address").value,
-                    amount: document.getElementById("amount").value * 100,
-                    currency: 'GHS',
-                    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
-                    // label: "Optional string that replaces customer email"
-                    onClose: function(){
-                    alert('Window closed.');
-                    },
-                    callback: function(response){
-                    let message = 'Payment complete! Reference: ' + response.reference;
-                    alert(message);
-                    },
-                    callback: function(response){
-                        let reference = response.reference;
-                        //verify payment
-                        console.log('/make-payment/' + document.getElementById("amount").value + '/' + reference);
-                        window.location.href=('/make-payment/' + document.getElementById("amount").value + '/' + {{$content->id}} + '/' + reference);
+                <!-- Fonts -->
+                <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+            </head>
+            <body >
+                <form id="paymentForm">
+                    <input value="abagale1994@gmail.com" type="email" id="email-address" hidden />
+                    <div class="form-group">
+                    <label for="amount">Amount</label>
+                    <input value="{{$content->price ?? 0.00}}" type="text" id="amount" disabled />
+                    </div>
+                    
+                    <div class="form-submit">
+                    <button type="submit" onclick="payWithPaystack(event)"> Pay </button>
+                    </div>
+                </form>
+                <script src="https://js.paystack.co/v1/inline.js"></script> 
+                <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+                <script>
+                    let paymentForm = document.getElementById('paymentForm');
+                    paymentForm.addEventListener("submit", payWithPaystack, false);
+                    function payWithPaystack(e) {
+                    e.preventDefault();
+                    let handler = PaystackPop.setup({
+                        key: 'pk_test_cef059417db5c00edff1ee70216ed36dd4ffb4e0', // Replace with your public key
+                        email: document.getElementById("email-address").value,
+                        amount: document.getElementById("amount").value * 100,
+                        currency: 'GHS',
+                        ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+                        // label: "Optional string that replaces customer email"
+                        onClose: function(){
+                        alert('Window closed.');
+                        },
+                        callback: function(response){
+                        let message = 'Payment complete! Reference: ' + response.reference;
+                        alert(message);
+                        },
+                        callback: function(response){
+                            let reference = response.reference;
+                            //verify payment
+                            console.log('/make-payment/' + document.getElementById("amount").value + '/' + reference);
+                            window.location.href=('/make-payment/' + document.getElementById("amount").value + '/' + {{$content->id}} + '/' + reference);
+                        }
+                    });
+                    handler.openIframe();
                     }
-                });
-                handler.openIframe();
-                }
-            </script>
-    </html>
+                </script>
+        </html>
+    </div>
 </x-app-layout>
--- resources/views/payout/index.blade.php
+++ resources/views/payout/index.blade.php
@@ -1,71 +1,73 @@
 <x-app-layout>
-    <!DOCTYPE html>
-    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-            <title>Pay Out </title>
-    
-            <!-- Fonts -->
-            <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        </head>
-        <body >
-            <form id="paymentForm">
+    <div class="p-24">
+        <!DOCTYPE html>
+        <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+                <title>Pay Out </title>
+        
+                <!-- Fonts -->
+                <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+            </head>
+            <body >
+                <form id="paymentForm">
+                    
+                    
+
+                    <div class="grid grid-cols-3 gap-6">
+                        <div class="col-span-3 sm:col-span-2">
+                            <label for="name" value="{{ __('payout.index') }}" class="block text-sm font-medium text-gray-700">Account Name</label>
+                            <div class="mt-1 flex rounded-md shadow-sm">
+                                <input name="name" id="contenttype" type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="john doe">           
+                            </div>
+                        </div>
+                    </div>  
+
+                    <div class="grid grid-cols-3 gap-6">
+                        <div class="col-span-3 sm:col-span-2">
+                            <label for="name" class="block text-sm font-medium text-gray-700">Email</label>
+                            <div class="mt-1 flex rounded-md shadow-sm">
+                                <input name="email" id="contenttype" type="email" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="joh@gmail.com">           
+                            </div>
+                        </div>
+                    </div> 
+
+                    <div class="grid grid-cols-3 gap-6">
+                        <div class="col-span-3 sm:col-span-2">
+                            <label for="name" value="{{ __('payout.index') }}" class="block text-sm font-medium text-gray-700"> Payout Platform</label>
+                            <div class="mt-1 flex rounded-md shadow-sm">
+                                <select name="platform" id="payout type" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md">
+                                    <option value="">Select</option>
+                                    <option value="0">Bank Transfer</option>
+                                    <option value="1">Mobile Money</option>
+                                </select>            
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="grid grid-cols-3 gap-6">
+                        <div class="col-span-3 sm:col-span-2">
+                            <label for="name" class="block text-sm font-medium text-gray-700">Title</label>
+                            <div class="mt-1 flex rounded-md shadow-sm">
+                                <input name="title" id="contenttype" type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Art/Music title">           
+                            </div>
+                        </div>
+                    </div> 
+
+                    <div class="grid grid-cols-3 gap-6">
+                        <div class="col-span-3 sm:col-span-2">
+                            <label for="name" class="block text-sm font-medium text-gray-700">Amount</label>
+                            <div class="mt-1 flex rounded-md shadow-sm">
+                                <input name="Amount" id="contenttype" type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="GHC 1000">           
+                            </div>
+                        </div>
+                    </div> 
                 
-                
-
-                <div class="grid grid-cols-3 gap-6">
-                    <div class="col-span-3 sm:col-span-2">
-                        <label for="name" value="{{ __('payout.index') }}" class="block text-sm font-medium text-gray-700">Account Name</label>
-                        <div class="mt-1 flex rounded-md shadow-sm">
-                            <input name="name" id="contenttype" type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="john doe">           
-                        </div>
-                    </div>
-                </div>  
-
-                <div class="grid grid-cols-3 gap-6">
-                    <div class="col-span-3 sm:col-span-2">
-                        <label for="name" class="block text-sm font-medium text-gray-700">Email</label>
-                        <div class="mt-1 flex rounded-md shadow-sm">
-                            <input name="email" id="contenttype" type="email" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="joh@gmail.com">           
-                        </div>
-                    </div>
-                </div> 
-
-                <div class="grid grid-cols-3 gap-6">
-                    <div class="col-span-3 sm:col-span-2">
-                        <label for="name" value="{{ __('payout.index') }}" class="block text-sm font-medium text-gray-700"> Payout Platform</label>
-                        <div class="mt-1 flex rounded-md shadow-sm">
-                            <select name="platform" id="payout type" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md">
-                                <option value="">Select</option>
-                                <option value="0">Bank Transfer</option>
-                                <option value="1">Mobile Money</option>
-                            </select>            
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="grid grid-cols-3 gap-6">
-                    <div class="col-span-3 sm:col-span-2">
-                        <label for="name" class="block text-sm font-medium text-gray-700">Title</label>
-                        <div class="mt-1 flex rounded-md shadow-sm">
-                            <input name="title" id="contenttype" type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Art/Music title">           
-                        </div>
-                    </div>
-                </div> 
-
-                <div class="grid grid-cols-3 gap-6">
-                    <div class="col-span-3 sm:col-span-2">
-                        <label for="name" class="block text-sm font-medium text-gray-700">Amount</label>
-                        <div class="mt-1 flex rounded-md shadow-sm">
-                            <input name="Amount" id="contenttype" type="text" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="GHC 1000">           
-                        </div>
-                    </div>
-                </div> 
-             
-            </form>
-    </html>
+                </form>
+        </html>
+    </div>
 </x-app-layout>   
     
 
