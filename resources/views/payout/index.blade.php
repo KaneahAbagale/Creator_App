13252eb12c490ee1e75b546b990b7fdda4dc8f91
KaneahAbagale <gertrude.kaneah.abagale@gmail.com>
2022-05-05 10:08:02 +0000

    added class to amount

--- resources/views/payout/index.blade.php
+++ resources/views/payout/index.blade.php
@@ -57,7 +57,7 @@
 
                 <div class="form-group">
                     <label for="amount">Amount</label>
-                    <input value="" type="text" id="amount" disabled />
+                    <input value="" type="text" id="amount" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" disabled />
                   </div>
              
             </form>
