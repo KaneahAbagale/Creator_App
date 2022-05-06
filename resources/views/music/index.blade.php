1dc29715f517c1e0754dec7382d9a10ec7cc30a5
KaneahAbagale <gertrude.kaneah.abagale@gmail.com>
2022-05-06 21:24:53 +0000

    added php syntax

--- resources/views/music/index.blade.php
+++ resources/views/music/index.blade.php
@@ -16,11 +16,12 @@
                   <h1 class="title-font text-lg font-medium text-gray-600 mb-3">{{$content->title}}</h1>
                   <p class="leading-relaxed mb-3">{{$content->price ?? 'free'}}</p>
                   <div class="flex items-center flex-wrap ">
-                    @if ($content->price = 0)
+                    <?php if ($content->price = 0) {
                       echo "<a href="/download" class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Download</a>"
-                      @else
+                    } else {
                       echo "<a href="payment.index/{{$content->id}}" class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Download</a>"
-                    @endif
+                    }
+                     ?>
                     {{-- <a href="payment.index/{{$content->id}}" class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Download</a> --}}
                   </div>
                 </div>
