b889435a16bba36aaf265b8fa970a3e3d6f6d573
KaneahAbagale <gertrude.kaneah.abagale@gmail.com>
2022-05-06 20:27:44 +0000

    updated href

--- resources/views/music/index.blade.php
+++ resources/views/music/index.blade.php
@@ -17,7 +17,7 @@
                   <p class="leading-relaxed mb-3">{{$content->price ?? 'free'}}</p>
                   <div class="flex items-center flex-wrap ">
                     @if ($content->price = 0.00))
-                      <a href="{{ route(‘/download’) }}" class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Download</a>
+                      <a href="/download" class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Download</a>
                       @else
                       <a href="payment.index/{{$content->id}}" class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Download</a>
                     @endif
