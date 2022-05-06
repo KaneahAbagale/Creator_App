dd6ebb41e003dbebd032bdfc9c5f5211986ebcc8
KaneahAbagale <gertrude.kaneah.abagale@gmail.com>
2022-05-06 21:33:56 +0000

    replaced if statement

--- resources/views/music/index.blade.php
+++ resources/views/music/index.blade.php
@@ -17,11 +17,11 @@
                   <p class="leading-relaxed mb-3">{{$content->price ?? 'free'}}</p>
                   <div class="flex items-center flex-wrap ">
                     <?php if ($content->price = 0) {
-                      echo '<a href="/download" class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Download</a>'
+                      echo "<a href='/download' class='bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg'>Download</a>"
                     } else {
-                      echo '<a href="payment.index/{{$content->id}}" class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Download</a>'
+                      echo "<a href='payment.index/{{$content->id}}' class='bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg'>Download</a>"
                     }
-                     ?>
+                    ?>
                     {{-- <a href="payment.index/{{$content->id}}" class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">Download</a> --}}
                   </div>
                 </div>
